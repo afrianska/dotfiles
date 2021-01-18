57c697b6f898c0dbec96dbe1655dd8663b41ca60
afrianska <lwx.shell@gmail.com>
2021-01-18 23:13:11 +0700

    add .file

---
+++ .xmonad/startup.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# Wallpaper
+if [ -z "$(pgrep nitrogen)" ] ; then
+    nitrogen --restore &
+fi
+
+# compton
+if [ -z "$(pgrep compton)" ] ; then
+    compton -b &
+fi
+
+# xbacklight
+if [ -z "$(pgrep xbacklight)" ] ; then
+    xbacklight -set 80% &
+fi
+
+
+
